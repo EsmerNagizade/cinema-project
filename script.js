19466f446b7ddd47504f3e11f14545521265cd11
Esmer Nagizade <esmer1234@gmail.com>
2024-05-25 22:06:33 +0400

    update

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 let firstSectionMain = document.querySelector('.first_section_main')
 let popular = document.querySelector('#popular')
 let best = document.querySelector('#best')
+let action = document.querySelector('#action')
 let img='https://image.tmdb.org/t/p/w500'
 
 let popularLeft=document.querySelector('.popular_left')
 let popularRight=document.querySelector('.popular_right')
 let bestleft=document.querySelector('.bestleft')
 let bestright=document.querySelector('.bestright')
+let secondSectionCards = document.querySelector('.second_section_cards')
+let firstSectionCards = document.querySelector('.first_section_cards')
+
+let actionLeft=document.querySelector('.actionleft')
+let actionRight=document.querySelector('.actionright')
+
+function getLocal(url) {
+    fetch(url)
+    .then(resp=>resp.json())
+    .then(arr=>arr.forEach(
+        x=>{
+            firstSectionCards.innerHTML+=`
+            <div class='first_section_card'>
+            <video controls src=${x.video} ></video>
+            <p>${x.name}</p>
+            </div>`
+        }
+    ))
+}
+
+
+
+window.addEventListener('load',()=>{
+    getLocal('./LocalApi.json')
+})
+
 
 
 
@@ -117,3 +144,52 @@ bestleft.addEventListener('click',()=>{
                 sliderr()
             }
     })
+
+    function getAction(url) {
+        fetch(url)
+    .then(resp=>resp.json())
+    .then(data=>data.results.forEach(a=>{
+        action.innerHTML+=`
+    <div class='action_Card'>
+    <img src=${img+a.backdrop_path}/>
+    <p>${a.title}</p>
+    </div>
+        `
+    }))}
+
+    window.addEventListener('load',()=>{
+        getAction('https://api.themoviedb.org/3/search/movie?api_key=1cf50e6248dc270629e802686245c2c8&query=action');
+    })
+
+
+
+    let actioncount=0
+
+function slideraction(){
+    for(let i=0; action.children.length;i++){
+        action.children[i].style.transform=`translateX(-${actioncount*300}px)`
+    }
+}
+
+actionLeft.addEventListener('click',()=>{
+    if(actioncount>0){
+        actioncount--
+        slideraction()
+    }
+    else{
+        actioncount=0
+        slideraction()
+    }
+})
+
+
+actionRight.addEventListener('click',()=>{
+    if(actioncount<action.children.length-4){
+        actioncount++
+        slideraction()
+    }
+    else{
+        actioncount=0
+        slideraction()
+    }
+})
